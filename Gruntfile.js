64688e5927dc012129b5b1ae31f2b4195c4da284
Thomas Winter <thomas@springload.co.nz>
2014-07-08 17:56:44 +1200

    first commit+

---
+++ Gruntfile.js
@@ -0,0 +1,102 @@
+/*
+ * grunt-grunticon-pigment
+ * https://github.com/springload/grunticon-pigment
+ *
+ * Copyright (c) 2014 Thomas Winter
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+
+        nodeunit: {
+            files: ['test/**/*_test.js']
+        },
+        watch: {
+            files: '<config:lint.files>',
+            tasks: 'default'
+        },
+
+
+        grunticon_pigment: {
+            foo: {
+                files: [{
+                    cwd: '<%= paths.grunticons %>',
+                    dest: '<%= paths.css %>'
+                }],
+                options: {
+
+                    // CSS filenames
+                    datasvgcss: "icons.data.svg.css",
+                    datapngcss: "icons.data.png.css",
+                    urlpngcss: "icons.fallback.css",
+
+                    // preview HTML filename
+                    previewhtml: "icons.preview.html",
+
+                    // grunticon loader code snippet filename
+                    loadersnippet: "grunticon.loader.js",
+
+                    // folder name (within cwd) for svg files
+                    svgFolder: "source",
+                    svgColorFolder: "source-colourise",
+
+                    // folder name (within dest) for png output
+                    pngfolder: "png",
+
+                    // prefix for CSS classnames
+                    cssprefix: ".i-",
+
+                    defaultWidth: "32px",
+                    defaultHeight: "32px",
+
+                    // colours for svg colourising
+                    svgColors: ["#ffffff", "#ff0000"],
+
+                    // css file path prefix - this defaults to "/" and will be placed before the "dest" path when stylesheets are loaded.
+                    // This allows root-relative referencing of the CSS. If you don't want a prefix path, set to to ""
+                    cssbasepath: "",
+                    customselectors: {
+                        "twitter": [".alternative-selector"]
+                    },
+
+                    // templates for css output and preview page
+                    template: "template/default-css.hbs",
+                    previewTemplate:  "template/preview-custom.hbs"
+
+                }
+            }
+        },
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/*.js',
+                'lib/*.js'
+            ],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
+
+        // Before generating any new files, remove any previously-created files.
+        clean: {
+            tests: ['tmp']
+        }
+    });
+
+    // Load local tasks.
+    grunt.loadTasks('tasks');
+
+    grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+    grunt.loadNpmTasks( 'grunt-contrib-nodeunit' );
+
+    // Default task.
+    grunt.registerTask('skip-tests', ['jshint', 'grunticon:foo']);
+    grunt.registerTask('travis', ['jshint', 'svgmin', 'grunticon:foo', 'nodeunit']);
+    grunt.registerTask('default', ['travis']);
+
+};
---
+++ tasks/grunticon_pigment.js
@@ -0,0 +1,202 @@
+/*
+ * grunt-grunticon-pigment
+ * https://github.com/springload/grunticon-pigment
+ *
+ * Copyright (c) 2014 Thomas Winter
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+    var path = require( 'path' );
+    var os = require( 'os' );
+
+    // load npmTasks from within plugin folder
+    var cwd = process.cwd();
+    process.chdir( path.join( __dirname, "..") );
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-grunticon');
+    grunt.loadNpmTasks('grunt-dom-munger');
+    grunt.loadNpmTasks('grunt-svgmin');
+    grunt.loadNpmTasks('grunt-text-replace');
+    process.chdir(cwd);
+
+    grunt.registerMultiTask("grunticon_pigment", "A collection of grunt tasks around filamentgroup's grunt-grunticon. Creates colourised versions of svg images.", function() {
+
+        // get the config
+        var config = this.options({
+            datasvgcss: "icons.data.svg.css",
+            datapngcss: "icons.data.png.css",
+            urlpngcss: "icons.fallback.css",
+            previewhtml: "preview.html",
+            customselectors: {},
+            cssprefix: ".i-",
+            defaultWidth: "32px",
+            defaultHeight: "32px",
+            colors: {},
+            pngfolder: "png",
+            pngpath: "",
+            template: "",
+            svgColors: [],
+            svgFolder: "source",
+            svgColorFolder: "source-colourise",
+            distDir: "grunticon-dist",
+            tmpDir: "grunticon-tmp",
+            previewTemplate: path.join( __dirname, "..", "templates", "preview.hbs" )
+        });
+
+        config.src = this.files[0].orig.cwd;
+        config.dest = this.files[0].orig.dest;
+
+        config.buildFolder = path.join( config.src, config.tmpDir );
+        config.distFolder = path.join( config.src, config.distDir );
+
+        // Pass config to our grunt tasks...
+        grunt.config.set('clean', {
+            icons: {
+                src: [ config.dest + "/" + config.pngfolder]
+            },
+            build: {
+                src: [ config.buildFolder, config.distFolder]
+            }
+        });
+        grunt.config.set('copy', {
+            icons: {
+                files: [
+                    {
+                        expand: true,
+                        src: config.src + '/' + config.svgFolder + '/*',
+                        dest: config.buildFolder,
+                        flatten: true
+                    }
+                ]
+            },
+            colourise_icons: {
+                files: [
+                    {
+                        expand: true,
+                        src: config.src + "/" + config.svgColorFolder + "/*",
+                        dest: config.buildFolder,
+                        flatten: true
+                    }
+                ]
+            },
+            grunticons: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: config.src,
+                        src: [config.distDir + '/*.css', config.distDir + '/*.html', config.distDir + '/png/*'],
+                        dest: config.dest,
+                        rename: function(dest, matchedSrcPath, options) {
+                            // var newSourcePath = matchedSrcPath.replace("/("+config.distDir+"\/")/,"");
+                            var re = new RegExp(config.distDir + "\/");
+                            var newSourcePath = matchedSrcPath.replace(re,"");
+                            return path.join(dest, newSourcePath);
+                        },
+                        flatten: false
+                    }
+                ]
+            }
+        });
+        grunt.config.set('svgmin', {
+            colourise_icons: {
+                files: [{
+                    expand: true,
+                    cwd: config.buildFolder,
+                    src: ['**/*.svg'],
+                    dest: config.buildFolder,
+                    ext: '.svg'
+                }]
+            },
+            icons: {
+                files: [{
+                    expand: true,
+                    cwd: config.src + '/' + config.svgFolder,
+                    src: ['**/*.svg'],
+                    dest: config.buildFolder,
+                    ext: '.svg'
+                }]
+            }
+        });
+
+        grunt.config.set('grunticon', {
+            icons: {
+                files: [{
+                    expand: true,
+                    cwd: config.buildFolder,
+                    src: ['*.svg', '*.png'],
+                    dest: config.distFolder
+                }],
+                options: config // pass the whole config
+            }
+        });
+
+        grunt.config.set('dom_munger', {
+            colourise_icons: {
+                options: {
+                    read: [
+                        {selector:'svg',attribute:'height',writeto:'iconHeight'},
+                        {selector:'svg',attribute:'width',writeto:'iconWidth'}
+                    ],
+                    update: {selector:'svg > *:first-child', attribute:'id', value:'a'},
+                    callback: function($){
+
+                        var colors = config.svgColors; //grunt.option('colours');
+                        var icon_count = colors.length+1; // include black icon without offset
+                        var icon_height = String(grunt.config("dom_munger.data.iconHeight")).replace("px","");
+                        var icon_width = String(grunt.config("dom_munger.data.iconWidth")).replace("px","");
+
+                        // remove all fill attributes, because they can't be overridden when we duplicate them
+                        $('svg *').removeAttr("fill");
+
+                        // set new document dimensions
+                        $('svg').attr("height", (icon_height*icon_count)+"px");
+                        $('svg').attr("viewBox", "0 0 " + icon_width + " " + (icon_height*icon_count) );
+
+                        // duplicate root node for each colour
+                        for (var i in colors) {
+                            var offset = icon_height * (parseFloat(i)+1);
+                            var color = colors[i];
+                            var xlink = "<use xlink:href=\"#a\" y=\""+offset+"\" fill=\""+color+"\" />";
+                            $('svg').append(xlink);
+                        }
+
+                    }
+                },
+                src: config.buildFolder + '/*.svg'
+            }
+        });
+
+        // fix css descendant selector for grunticon's customselectors
+        grunt.config.set('replace', {
+            icons: {
+                src: config.distFolder + '/*.css',
+                overwrite: true,
+                replacements: [
+                    {
+                        from: "&gt;",
+                        to: ">"
+                    }
+                ]
+            }
+        });
+
+        grunt.task.run('clean:icons');
+        grunt.task.run('copy:colourise_icons');
+        grunt.task.run('dom_munger:colourise_icons');
+        grunt.task.run('svgmin:icons');
+        grunt.task.run('copy:icons');
+        grunt.task.run('grunticon:icons');
+        grunt.task.run('replace:icons');
+        grunt.task.run('copy:grunticons');
+        grunt.task.run('clean:build');
+
+    });
+
+
+
+};
